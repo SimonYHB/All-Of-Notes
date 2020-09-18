896102047fa1a066b82deb5a807f359b454f4f17
SimonYe <yehuangbin@hotmail.com>
2020-09-18 10:50:12 +0800

    update: 更新算法笔记

--- note/算法笔记/Algorithm/Algorithm/剑指Offer/13机器人的运动范围.swift
+++ note/算法笔记/Algorithm/Algorithm/剑指Offer/13机器人的运动范围.swift
@@ -27,8 +27,47 @@
  */
 import Foundation
 /*
- 思路：与12矩阵中的路径解法类似，列出所有可能值
+ 思路1：与12矩阵中的路径解法类似，使用dfs，列出所有方向的可能值，并使用备忘录记录走过的值
+ 也可以采用BFS广度优先搜索，先从行开始+1直到不满足要求，再从列开始
  */
 func movingCount(_ m: Int, _ n: Int, _ k: Int) -> Int {
-    return 0
+    var visited = Array(repeating: Array(repeating: false, count: n), count: m)
+    let count = movingCount_dfs(m, n, k, 0, 0, &visited)
+    return count
 }
+
+
+
+func movingCount_dfs(_ m: Int, _ n: Int, _ k: Int, _ row: Int, _ col: Int, _ visited: inout [[Bool]]) -> Int {
+    var count = 0
+    /*
+     判断条件：
+     1. 未越界
+     2. 未访问过
+     3. 满足相加小于等于k
+     */
+    if (row < m && row >= 0 && col < n && col >= 0
+        && !visited[row][col]
+        && numCharSum(row) + numCharSum(col) <= k) {
+        visited[row][col] = true
+        count = 1 + movingCount_dfs(m, n, k, row+1, col, &visited)
+            + movingCount_dfs(m, n, k, row-1, col, &visited)
+            + movingCount_dfs(m, n, k, row, col+1, &visited)
+            + movingCount_dfs(m, n, k, row, col-1, &visited)
+    }
+
+    
+    return count
+}
+
+
+func numCharSum(_ num:Int) -> Int {
+    var sum = 0
+    var num = num
+    while num > 0 {
+        sum += num % 10
+        num = num/10
+    }
+    return sum
+}
+
